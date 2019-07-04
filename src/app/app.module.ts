ea235d07a5c1731d0c8ffc0e2532566ebc4c9aeb
Carlos Vaquedano <chvb2002@hotmail.com>
2019-07-03 22:09:02 -0600

    Store dev tool

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,9 @@ import { AppComponent } from './app.component';
 import { HijoComponent } from './contador/hijo/hijo.component';
 import { NietoComponent } from './contador/nieto/nieto.component';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { contadorReducer } from './contador/contador.reducer';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,11 @@ import { contadorReducer } from './contador/contador.reducer';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({contador: contadorReducer})
+    StoreModule.forRoot({contador: contadorReducer}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
